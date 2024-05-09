8a25064ef77f4ac84a6db1eb55dcdcace440400b
asarr22 <msarr3334@gmail.com>
2024-05-09 00:41:35 +0000

    Refactor app initialization: Use initState and remove UncontrolledProviderScope Refactored the app initialization process by:
    1. Moving the initialSetup call of FirebaseAuthService to the initState method of the App StatefulWidget, ensuring it is executed exactly once at app startup.
    2. Removing UncontrolledProviderScope to simplify provider management using the standard ProviderScope, enhancing clarity and maintainability.
    
    These changes streamline the initialization sequence and improve the overall structure and reliability of the app’s startup process.

--- lib/app.dart
+++ lib/app.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:gshopp_flutter/common/repositories/auth_services.dart';
 import 'package:gshopp_flutter/features/authentication/screens/emailconfirmation/emil_success.dart';
 import 'package:gshopp_flutter/features/authentication/screens/emailconfirmation/verify_email_page.dart';
 import 'package:gshopp_flutter/features/authentication/screens/forgot_password/forget_password.dart';
@@ -12,17 +13,24 @@ import 'package:gshopp_flutter/features/shell/appshell.dart';
 import 'package:gshopp_flutter/utils/theme/theme.dart';
 import 'package:gshopp_flutter/utils/theme/theme_mode.dart';
 import 'package:gshopp_flutter/utils/helpers/helper_fuctions.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
-final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
-  throw UnimplementedError(); // This will be overridden
-});
-
-class App extends ConsumerWidget {
+class App extends ConsumerStatefulWidget {
   const App({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<App> createState() => _AppState();
+}
+
+class _AppState extends ConsumerState<App> {
+  @override
+  void initState() {
+    // Run Firebase Authentication Service
+    ref.read(firebaseAuthServiceProvider).initialSetup();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final appThemeMode = ref.watch(themeModeProvider);
     ThemeMode themeMode;
     switch (appThemeMode) {
--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,25 @@ import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:gshopp_flutter/app.dart';
 import 'package:gshopp_flutter/common/repositories/auth_services.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
+  throw UnimplementedError(); // This will be overridden in main
+});
+
+final firebaseAuthServiceProvider = Provider<FirebaseAuthService>((ref) {
+  final SharedPreferences prefs = ref.watch(sharedPreferencesProvider);
+  return FirebaseAuthService(prefs); // Assuming FirebaseAuthService needs SharedPreferences
+});
+
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  final WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-  final sharedPreferences = await SharedPreferences.getInstance();
+  final SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
 
-  // Firebase Initialization
   if (kIsWeb || Platform.isAndroid) {
     await Firebase.initializeApp(
         options: const FirebaseOptions(
@@ -26,26 +32,7 @@ Future main() async {
     await Firebase.initializeApp();
   }
 
-  // Setup ProviderContainer
-  final container = ProviderContainer(overrides: [
+  runApp(ProviderScope(overrides: [
     sharedPreferencesProvider.overrideWithValue(sharedPreferences),
-  ]);
-
-  // Access FirebaseAuthService through the container
-  final firebaseAuthService = container.read(firebaseAuthServiceProvider);
-
-  // Await Splash until other items Load
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
-
-  // init Firebase Cloud Storage
-  await Firebase.initializeApp();
-  runApp(
-    UncontrolledProviderScope(
-      container: container,
-      child: const App(),
-    ),
-  );
-
-  // Execute initial setup logic
-  await firebaseAuthService.initialSetup();
+  ], child: const App()));
 }
