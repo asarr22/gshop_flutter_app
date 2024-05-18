d04a80acc76ab008c5c1b829f54e408587091403
asarr22 <msarr3334@gmail.com>
2024-05-18 13:41:23 +0000

    build: fix MultiDexApplication error while building

--- android/app/src/main/java/io/flutter/app/FlutterMultiDexApplication.java
+++ android/app/src/main/java/io/flutter/app/FlutterMultiDexApplication.java
@@ -11,11 +11,12 @@ import android.app.Application;
 import android.content.Context;
 import androidx.annotation.CallSuper;
 import androidx.multidex.MultiDex;
+import androidx.multidex.MultiDexApplication;
 
 /**
  * Extension of {@link android.app.Application}, adding multidex support.
  */
-public class FlutterMultiDexApplication extends Application {
+public class FlutterMultiDexApplication extends MultiDexApplication {
   @Override
   @CallSuper
   protected void attachBaseContext(Context base) {
